44230a05285c8124c6c4b5e639b44424f2545027
Martin Benkov <martinbenkov@gmail.com>
2016-10-21 05:59:51 +0300

    Split into interfaces

--- src/main/java/com/example/runnable/EmailSendingRouter.java
+++ src/main/java/com/example/runnable/EmailSendingRouter.java
@@ -1,122 +1,17 @@
 package com.example.runnable;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.atomic.AtomicLong;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Component;
-
-import com.example.configuration.ConfigurationManager;
 import com.example.domain.Email;
-import com.example.routing.RoutingStratagy;
 import com.example.senders.EmailSenderManager;
-import com.example.senders.EmailSenderManagerFactory;
-import com.example.senders.EmailSenderType;
 
-@Component
-public class EmailSendingRouter implements Runnable, InitializingBean
+public interface EmailSendingRouter 
 {
-	private final static Logger logger = LoggerFactory.getLogger(EmailSendingRouter.class);
+	public void dispatchToManagers(Email email);
 	
-	@Autowired
-	@Qualifier("RoundRobinRoutingStratagy")
-	private RoutingStratagy routingStratagy;
+	public List<EmailSenderManager> getEmailSenderManagers();
 	
-	@Autowired
-	private ConfigurationManager configurationManager;
+	public long getNotQueuedCount();
 	
-	private BlockingQueue<Email> queue;
-	
-	private ExecutorService executorService;
-	
-	private List<EmailSenderManager> emailSenderManagers;
-	
-	private AtomicLong notQueuedCount;
-	
-	public EmailSendingRouter()
-	{
-	}
-	
-	@Override
-	public void afterPropertiesSet() throws Exception
-	{
-		int queueSize = configurationManager.getEmailQueueSize();
-		int sendsersOfOneTypeCount = configurationManager.getSendsersOfOneTypeCount();
-		
-		notQueuedCount = new AtomicLong();
-		emailSenderManagers = new ArrayList<EmailSenderManager>();
-		queue = new ArrayBlockingQueue<Email>(queueSize);
-		int threadPoolSize = sendsersOfOneTypeCount * 2;
-		executorService = Executors.newFixedThreadPool(threadPoolSize);
-		for (int i=0; i< sendsersOfOneTypeCount; i++)
-		{
-			EmailSenderManager mailgunSenderManager = EmailSenderManagerFactory.
-					create(EmailSenderType.MAILGUN, queueSize);
-			emailSenderManagers.add(mailgunSenderManager);
-			executorService.submit((Runnable)mailgunSenderManager);
-			
-			EmailSenderManager sendgridSenderManager = EmailSenderManagerFactory.
-					create(EmailSenderType.SENDGRID, queueSize);
-			emailSenderManagers.add(sendgridSenderManager);
-			executorService.submit((Runnable)sendgridSenderManager);
-		}
-		
-	}
-	
-	public void dispatchToManagers(Email email)
-	{
-		boolean added = queue.offer(email);
-		
-		if (!added)
-		{
-			notQueuedCount.incrementAndGet();
-			logger.error("Cannot enqueue email " + email.getId());
-		}
-		
-	}
-
-
-	public void run()
-	{
-		while (true)
-		{
-			try
-			{
-				Email email = queue.take();
-				EmailSenderManager emailSenderManager = routingStratagy.getEmailSender(emailSenderManagers);
-				emailSenderManager.sendToProviderQueue(email);
-			}
-			catch (Throwable t)
-			{
-				logger.debug("error", t);
-				
-			}
-		}
-	}
-	
-	public List<EmailSenderManager> getEmailSenderManagers()
-	{
-		return emailSenderManagers;
-	}
-	
-	public long getNotQueuedCount()
-	{
-		return notQueuedCount.longValue();
-	}
-	
-	public int getInQueueCount()
-	{
-		return queue.size();
-	}
-
-
+	public int getInQueueCount();
 }
