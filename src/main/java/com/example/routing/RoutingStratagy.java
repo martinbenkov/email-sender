c46b82cd81446c3204e3dbca8f2bf224a72a25fd
martinbenkov <martinbenkov@gmail.com>
2016-10-21 05:35:43 +0300

    Update RoutingStratagy.java

--- src/main/java/com/example/routing/RoutingStratagy.java
+++ src/main/java/com/example/routing/RoutingStratagy.java
@@ -7,5 +7,10 @@ import com.example.senders.EmailSenderManager;
 
 public interface RoutingStratagy
 {
+	/**
+	 * 
+	 * @param emailSenderManagers - list of EmailSenderManager
+	 * @return the most appropriate EmailSenderManager, regarding the stratagy
+	 */
 	public EmailSenderManager getEmailSender(List<EmailSenderManager> emailSenderManagers);
 }
